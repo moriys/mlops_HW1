c24fd79760441dab76dc55fca541219bb5ab3591
username <for_studing@yandex.ru>
2022-11-19 20:06:00 +0300

    Boston housing raw train and test datasets was added

--- scripts/data_scripts/get_data.py
+++ scripts/data_scripts/get_data.py
@@ -9,6 +9,6 @@ boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
 boston['MEDV'] = boston_dataset.target
 boston = shuffle(boston)
 
-boston.iloc[:400].to_csv("data/raw/train.csv", index=False)
-boston.iloc[401:].to_csv("data/raw/test.csv", index=False)
+boston.iloc[:400].to_csv("../../data/raw/train.csv", index=False)
+boston.iloc[401:].to_csv("../../data/raw/test.csv", index=False)
 
